95dda5712645fde615f510355a1116eec1eaaedf
Jan K. Szymanski <Jan.Szymanski@dyson.com>
2019-12-09 14:22:27 +0000

    Coroutines codelab finished

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -19,8 +19,10 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.BACKGROUND
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.*
 
 /**
  * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
@@ -101,11 +103,10 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      * Wait one second then update the tap count.
      */
     private fun updateTaps() {
-        // TODO: Convert updateTaps to use coroutines
-        tapCount++
-        BACKGROUND.submit {
-            Thread.sleep(200)
-            _taps.postValue("${tapCount} taps")
+        viewModelScope.launch {
+            tapCount++
+            delay(1_000)
+            _taps.postValue("$tapCount taps")
         }
     }
 
@@ -120,17 +121,19 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      * Refresh the title, showing a loading spinner while it refreshes and errors via snackbar.
      */
     fun refreshTitle() {
-        // TODO: Convert refreshTitle to use coroutines
-        _spinner.value = true
-        repository.refreshTitleWithCallbacks(object: TitleRefreshCallback {
-            override fun onCompleted() {
-                _spinner.postValue(false)
-            }
+        launchDataLoad {
+            repository.refreshTitle()
+        }
+    }
 
-            override fun onError(cause: Throwable) {
-                _snackBar.postValue(cause.message)
-                _spinner.postValue(false)
-            }
-        })
+    private fun launchDataLoad(block: suspend () -> Unit): Job = viewModelScope.launch {
+        try {
+            _spinner.value = true
+            block()
+        } catch (e: TitleRefreshError) {
+            _snackBar.value = e.message
+        } finally {
+            _spinner.value = false
+        }
     }
 }
